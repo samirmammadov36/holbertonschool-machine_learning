10df721535dcadf4dbb726c19cc37bee6396073a
Samir Mammadov <161446409+SamirMagb111111@users.noreply.github.com>
2026-05-16 18:47:49 +0400

    Task

--- math/multivariate_prob/multinormal.py
+++ math/multivariate_prob/multinormal.py
@@ -29,3 +29,35 @@ class MultiNormal:
         data_centered = data - self.mean
 
         self.cov = np.matmul(data_centered, data_centered.T) / (n - 1)
+
+    def pdf(self, x):
+        """
+        Calculates the PDF at a data point.
+
+        Args:
+            x: numpy.ndarray of shape (d, 1)
+
+        Returns:
+            float: PDF value
+        """
+        if not isinstance(x, np.ndarray):
+            raise TypeError("x must be a numpy.ndarray")
+
+        d = self.mean.shape[0]
+
+        if x.shape != (d, 1):
+            raise ValueError("x must have the shape ({}, 1)".format(d))
+
+        cov_det = np.linalg.det(self.cov)
+        cov_inv = np.linalg.inv(self.cov)
+
+        diff = x - self.mean
+
+        exponent = -0.5 * np.matmul(
+            np.matmul(diff.T, cov_inv),
+            diff
+        )[0][0]
+
+        denominator = np.sqrt(((2 * np.pi) ** d) * cov_det)
+
+        return (1 / denominator) * np.exp(exponent)
