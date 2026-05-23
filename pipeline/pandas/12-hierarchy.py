b1d4e7d9acb169a8b31fcbb448ae7bbffc4aba08
Samir Mammadov <161446409+SamirMagb111111@users.noreply.github.com>
2026-05-23 23:36:12 +0400

    Task

---
+++ pipeline/pandas/12-hierarchy.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""Concatenate DataFrames with hierarchical index."""
+
+import pandas as pd
+
+index = __import__("10-index").index
+
+
+def hierarchy(df1, df2):
+    """Create hierarchical DataFrame ordered by timestamp."""
+    df1 = index(df1)
+    df2 = index(df2)
+
+    df1 = df1.loc[1417411980:1417417980]
+    df2 = df2.loc[1417411980:1417417980]
+
+    df = pd.concat([df2, df1], keys=["bitstamp", "coinbase"])
+    df = df.swaplevel(0, 1)
+    df = df.sort_index()
+
+    return df
