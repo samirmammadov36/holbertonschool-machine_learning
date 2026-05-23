483f063370abe5d77f7212775318d9f867aed369
Samir Mammadov <161446409+SamirMagb111111@users.noreply.github.com>
2026-05-24 00:05:03 +0400

    Task

---
+++ pipeline/pandas/14-visualize.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""Visualize a pandas DataFrame."""
+
+import matplotlib.pyplot as plt
+import pandas as pd
+from_file = __import__('2-from_file').from_file
+
+df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
+
+df = df.drop(columns=["Weighted_Price"])
+df = df.rename(columns={"Timestamp": "Date"})
+df["Date"] = pd.to_datetime(df["Date"], unit="s")
+df = df.set_index("Date")
+
+df["Close"] = df["Close"].ffill()
+df["High"] = df["High"].fillna(df["Close"])
+df["Low"] = df["Low"].fillna(df["Close"])
+df["Open"] = df["Open"].fillna(df["Close"])
+df["Volume_(BTC)"] = df["Volume_(BTC)"].fillna(0)
+df["Volume_(Currency)"] = df["Volume_(Currency)"].fillna(0)
+
+df = df["2017":]
+
+df = df.resample("D").agg({
+    "High": "max",
+    "Low": "min",
+    "Open": "mean",
+    "Close": "mean",
+    "Volume_(BTC)": "sum",
+    "Volume_(Currency)": "sum"
+})
+
+print(df)
+
+df.plot()
+plt.show()
