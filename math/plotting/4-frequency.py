d58d63fc4769aa6f62f56cdd4bba1ebc9068f5a1
Samir Mammadov <161446409+SamirMagb111111@users.noreply.github.com>
2026-05-30 23:19:58 +0400

    Task

---
+++ math/plotting/4-frequency.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""This module plots a histogram of student grades."""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def frequency():
+    """Plot a histogram showing the frequency of student grades."""
+    np.random.seed(5)
+    student_grades = np.random.normal(68, 15, 50)
+    plt.figure(figsize=(6.4, 4.8))
+
+    plt.hist(student_grades, bins=range(0, 101, 10), edgecolor='black')
+    plt.xlabel('Grades')
+    plt.ylabel('Number of Students')
+    plt.title('Project A')
+
+    plt.show()
