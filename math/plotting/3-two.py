aad11d10833bb4dbb8cbf2148a0428c4b674cb2c
Samir Mammadov <161446409+SamirMagb111111@users.noreply.github.com>
2026-05-30 23:19:03 +0400

    Task

---
+++ math/plotting/3-two.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""This module plots two radioactive decay graphs."""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def two():
+    """Plot the exponential decay of C-14 and Ra-226."""
+    x = np.arange(0, 21000, 1000)
+    r = np.log(0.5)
+    t1 = 5730
+    t2 = 1600
+    y1 = np.exp((r / t1) * x)
+    y2 = np.exp((r / t2) * x)
+    plt.figure(figsize=(6.4, 4.8))
+
+    plt.plot(x, y1, 'r--', label='C-14')
+    plt.plot(x, y2, 'g-', label='Ra-226')
+    plt.xlabel('Time (years)')
+    plt.ylabel('Fraction Remaining')
+    plt.title('Exponential Decay of Radioactive Elements')
+    plt.xlim(0, 20000)
+    plt.ylim(0, 1)
+    plt.legend(loc='upper right')
+
+    plt.show()
