2bad00cae8c80c8aff5e35c51708a726d03aa452
Samir Mammadov <161446409+SamirMagb111111@users.noreply.github.com>
2026-05-30 23:21:57 +0400

    Task

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -11,7 +11,7 @@ def frequency():
     student_grades = np.random.normal(68, 15, 50)
     plt.figure(figsize=(6.4, 4.8))
 
-    plt.hist(student_grades, bins=range(0, 101, 10), edgecolor='black')
+    plt.hist(student_grades, bins=range(0, 110, 10), edgecolor='black')
     plt.xlabel('Grades')
     plt.ylabel('Number of Students')
     plt.title('Project A')
